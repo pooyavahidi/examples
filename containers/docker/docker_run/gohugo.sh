52db1a1e086d5e3de8cb0ab9b0f61966d85b9310
pooyavahidi <56851682+pooyavahidi@users.noreply.github.com>
2022-07-30 13:48:52 +1000

    feat: add docker run examples for hugo

---
+++ containers/docker/docker_run/gohugo.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+function d-hugo-run() {
+    local __hugo_workspace
+    __hugo_workspace=$1
+
+    [[ -z $1 ]] && __hugo_workspace=$(pwd)
+
+    docker container run --rm -p 1313:1313 \
+        -v $__hugo_workspace:/home/dev/workspace \
+        pv/hugo:latest \
+        hugo -D server -s workspace --bind 0.0.0.0
+}
+
+function dsh-hugo() {
+    local __hugo_workspace
+    __hugo_workspace=$1
+
+    [[ -z $1 ]] && __hugo_workspace=$(pwd)
+
+    docker container run -it --rm -p 1314:1313 \
+        -v $__hugo_workspace:/home/dev/workspace \
+        pv/hugo-shell:latest \
+        zsh
+}
+
+function d-hugo-build {
+    local __hugo_workspace
+    __hugo_workspace=$1
+    [[ -z $1 ]] && __hugo_workspace=$(pwd)
+
+    docker container run --rm \
+        -v $__hugo_workspace:/home/dev/workspace \
+        pv/hugo:latest \
+        hugo -s workspace
+
+}
