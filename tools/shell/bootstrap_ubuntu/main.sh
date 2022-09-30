789b00e696aac80413d7ebbe3a106cbbc065274a
pooyavahidi <56851682+pooyavahidi@users.noreply.github.com>
2022-09-30 11:30:29 +1000

    add install_docker to bootstrap_ubuntu script

--- tools/shell/bootstrap_ubuntu/main.sh
+++ tools/shell/bootstrap_ubuntu/main.sh
@@ -111,6 +111,34 @@ function install_golang() {
     echo 'export PATH="/usr/local/go/bin:$GOPATH/bin:$PATH"' >> ${HOME}/.extra
 }
 
+#######################################
+# Install Docker
+# Arguments:
+#   None
+########################################
+function install_docker() {
+
+    set -eux; \
+    sudo apt-get update; \
+    sudo apt-get install -y \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release; \
+    mkdir -p /etc/apt/keyrings; \
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg; \
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null; \
+
+   sudo apt-get update; \
+   sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin; \
+    \
+    # Setup docker user and group
+    sudo usermod -aG docker $USER; \
+    docker --version; \
+    docker compose version;
+}
+
 #########################################
 # Run the functions in order
 # Arguments:
@@ -118,10 +146,17 @@ function install_golang() {
 ########################################
 function main() {
   export DEBIAN_FRONTEND=noninteractive
+
   install_prerequisites
   install_awscli
   install_golang
+  install_docker
+
+  # Final update and upgrade
   sudo apt update && sudo apt upgrade -y
+
+  echo --------------------------
+  echo zsh shell will be active from next login
 }
 
 main "${@}"
