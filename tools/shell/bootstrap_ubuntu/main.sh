98eeed7bc33833bef2e83f28f92048de111c5fdf
pooyavahidi <56851682+pooyavahidi@users.noreply.github.com>
2022-09-30 15:17:23 +1000

    add install_nodejs to bootstrap_ubuntu script

--- tools/shell/bootstrap_ubuntu/main.sh
+++ tools/shell/bootstrap_ubuntu/main.sh
@@ -6,7 +6,6 @@
 # Install prerequisite tools and packages
 #######################################
 function install_prerequisites() {
-  set -eux; \
   sudo apt update; \
   sudo apt install -y \
         dirmngr \
@@ -43,12 +42,10 @@ function install_prerequisites() {
     sudo chsh $USER -s $(which zsh);
 }
 
-
 #######################################
 # Install awscli v2
 #######################################
 function install_awscli() {
-    set -eux; \
     cd $HOME/workspace/temp; \
     dpkgArch=$(dpkg --print-architecture); \
     case "${dpkgArch##-}" in \
@@ -71,12 +68,10 @@ function install_awscli() {
     aws --version;
 }
 
-
 #######################################
 # Install golang
 #######################################
 function install_golang() {
-    set -eux; \
     export GOLANG_VERSION=1.19.1; \
     dpkgArch=$(dpkg --print-architecture); \
     case "${dpkgArch##-}" in \
@@ -117,8 +112,6 @@ function install_golang() {
 #   None
 ########################################
 function install_docker() {
-
-    set -eux; \
     sudo apt-get update; \
     sudo apt-get install -y \
         ca-certificates \
@@ -139,24 +132,76 @@ function install_docker() {
     docker compose version;
 }
 
+#######################################
+# Install nodejs
+# Arguments:
+#   None
+########################################
+function install_nodejs() {
+    export NODE_VERSION=16.17.1 \
+    && ARCH= && dpkgArch="$(dpkg --print-architecture)" \
+    && case "${dpkgArch##*-}" in \
+      amd64) ARCH='x64';; \
+      ppc64el) ARCH='ppc64le';; \
+      s390x) ARCH='s390x';; \
+      arm64) ARCH='arm64';; \
+      armhf) ARCH='armv7l';; \
+      i386) ARCH='x86';; \
+      *) echo "unsupported architecture"; exit 1 ;; \
+    esac \
+    # gpg keys listed at https://github.com/nodejs/node#release-keys
+    && for key in \
+      4ED778F539E3634C779C87C6D7062848A1AB005C \
+      141F07595B7B3FFE74309A937405533BE57C7D57 \
+      94AE36675C464D64BAFA68DD7434390BDBE9B9C5 \
+      74F12602B6F1C4E913FAA37AD3A89613643B6201 \
+      71DCFD284A79C3B38668286BC97EC7A07EDE3FC1 \
+      61FC681DFB92A079F1685E77973F295594EC4689 \
+      8FCCA13FEF1D0C2E91008E09770F7A9A5AE15600 \
+      C4F0DFFF4E8C1A8236409D08E73BC641CC11F4C8 \
+      890C08DB8579162FEE0DF9DB8BEAB4DFCF555EF4 \
+      C82FA3AE1CBEDC6BE46B9360C43CEC45C17AB93C \
+      DD8F2338BAE7501E3DD5AC78C273792F7D83545D \
+      A48C2BEE680E841632CD4E44F07496B3EB3C1762 \
+      108F52B48DB57BB0CC439B2997B01419BD92F80A \
+      B9E2F5981AA6E0CD28160D9FF13993A75599653C \
+    ; do \
+        gpg --batch --keyserver hkps://keys.openpgp.org --recv-keys "$key" || \
+        gpg --batch --keyserver keyserver.ubuntu.com --recv-keys "$key" ; \
+    done \
+    && curl -fsSLO --compressed "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-$ARCH.tar.xz" \
+    && curl -fsSLO --compressed "https://nodejs.org/dist/v$NODE_VERSION/SHASUMS256.txt.asc" \
+    && gpg --batch --decrypt --output SHASUMS256.txt SHASUMS256.txt.asc \
+    && grep " node-v$NODE_VERSION-linux-$ARCH.tar.xz\$" SHASUMS256.txt | sha256sum -c - \
+    && tar -xJf "node-v$NODE_VERSION-linux-$ARCH.tar.xz" -C /usr/local --strip-components=1 --no-same-owner \
+    && rm "node-v$NODE_VERSION-linux-$ARCH.tar.xz" SHASUMS256.txt.asc SHASUMS256.txt \
+    && ln -s /usr/local/bin/node /usr/local/bin/nodejs \
+    # Test the installation
+    && node --version \
+    && npm --version
+}
+
 #########################################
 # Run the functions in order
 # Arguments:
 #   command line
 ########################################
 function main() {
-  export DEBIAN_FRONTEND=noninteractive
+    set -eux
+    export DEBIAN_FRONTEND=noninteractive
 
-  install_prerequisites
-  install_awscli
-  install_golang
-  install_docker
+    install_prerequisites
+    install_awscli
+    install_golang
+    install_nodejs
+    install_docker
 
-  # Final update and upgrade
-  sudo apt update && sudo apt upgrade -y
+    # Final update and upgrade
+    sudo apt update && sudo apt upgrade -y
 
-  echo --------------------------
-  echo zsh shell will be active from next login
+    echo --------------------------
+    echo zsh shell will be active from next login
 }
 
 main "${@}"
+
