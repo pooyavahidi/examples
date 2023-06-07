91b4af70e47a1e62f5067ca8c55cf1fb1de8ece6
pooyavahidi <56851682+pooyavahidi@users.noreply.github.com>
2023-06-07 21:09:24 +1000

    update math/probability/weighted_distribution

--- math/probability/weighted_distribution.py
+++ math/probability/weighted_distribution.py
@@ -1,13 +1,29 @@
 import time
 import numpy as np
 
-elements = ["A", "B", "C", "D", "E"]
-weights = [1, 2, 3, 5, 100]
+M = np.array(
+    [
+        ["A", 1],
+        ["B", 2],
+        ["C", 3],
+        ["D", 5],
+        ["E", 10],
+    ]
+)
+
+elements = M[:, 0]  # All rows, first column
+weights = M[:, 1].astype(int)  # All rows, second column, convert to integer
+
 probabilities = weights / np.sum(weights)
 
 
 def draw_element(elements, probabilities):
     return np.random.choice(elements, p=probabilities)
+
+
+def print_probabilities():
+    for i in range(len(elements)):
+        print(f"{elements[i]}: {probabilities[i]*100}%")
 
 
 def wheel_of_fortune():
@@ -22,4 +38,5 @@ def wheel_of_fortune():
 
 
 if __name__ == "__main__":
+    print_probabilities()
     wheel_of_fortune()
