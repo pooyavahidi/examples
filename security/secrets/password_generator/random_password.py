7124a0f328574d4487a32a33e6ea9863bf2690d1
pooyavahidi <56851682+pooyavahidi@users.noreply.github.com>
2023-08-26 09:52:35 +1000

    feat: improve random_password.py

--- security/secrets/password_generator/random_password.py
+++ security/secrets/password_generator/random_password.py
@@ -1,58 +1,59 @@
 import string
 import secrets
-import sys
+import argparse
+
 
 class RandomPassword:
-
-    # Some examples of supported and unsupported punctuations list
-    quotations = ["'",'"']
-    unsupported_punctuation_1 = ['<','>',';',"\\"]
-    unsupported_punctuation_2 = ['!','@','#','$','%','^','&','*','(',')','.']
-    supported_punctuation_1 = ['.', '-','_']
+    # Categorize the punctuations into meaningful groups
+    quotations = ["'", '"', "`"]
+    brackets = ["<", ">", "(", ")", "[", "]", "{", "}"]
+    slashes = ["/", "\\"]
+    colons = [":", ";"]
+    marks = ["!", "?", ".", ","]
+    special_chars = [
+        "@",
+        "#",
+        "$",
+        "%",
+        "^",
+        "&",
+        "*",
+        "+",
+        "=",
+        "~",
+        "|",
+    ]
+    safe_chars = ["-", "_"]
 
     def __init__(
         self,
-        length = 20,
-        number_occurrences = 2,
-        uppercase_occurrences = 2,
-        lowercase_occurrences = 2,
-        punctuation_occurrences = 2,
+        length=20,
+        number_occurrences=2,
+        uppercase_occurrences=2,
+        lowercase_occurrences=2,
+        punctuation_occurrences=4,
         space_occurrences=0,
-        exclude_characters=""):
-
+        exclude_groups=[],
+        include_groups=[],
+        alphanumeric=False,
+        use_safe_chars=False,
+    ):
         self.length = length
         self.number_occurrences = number_occurrences
         self.uppercase_occurrences = uppercase_occurrences
         self.lowercase_occurrences = lowercase_occurrences
         self.punctuation_occurrences = punctuation_occurrences
         self.space_occurrences = space_occurrences
-        self.exclude_characters = exclude_characters
+        self.exclude_groups = exclude_groups
+        self.include_groups = include_groups
+        self.alphanumeric = alphanumeric
+        self.use_safe_chars = use_safe_chars
 
-        self.max_retry = 1000
-
+        self.max_retry = 10000
 
     def generate(self):
+        characters = self._get_characters()
 
-        characters = []
-        if self.number_occurrences > 0:
-            characters += string.digits
-        if self.uppercase_occurrences > 0:
-            characters += string.ascii_uppercase
-        if self.lowercase_occurrences > 0:
-            characters += string.ascii_lowercase
-        if self.punctuation_occurrences > 0:
-            characters += string.punctuation
-        if self.space_occurrences > 0:
-            characters += " "
-
-        characters = [x for x in characters if x not in self.exclude_characters]
-        # TODO: After excluding the characters, still we need to check if based on
-        # the existing characters, can we generate a password which meets the
-        # occurrences conditions (in a reasonable time period). As a workaround, we
-        # can rely on max number of retry and then ask consumer to relax the conditions
-
-        # check the password and if the occurrence conditions have not been met
-        # retry and generate a new password
         retry_counter = 1
         while True:
             password = "".join(
@@ -61,56 +62,109 @@ class RandomPassword:
             if self._check_password(password):
                 break
 
-            # raise an error if after maximum tries still password cannot
-            # be generated under given conditions
             retry_counter += 1
             if retry_counter == self.max_retry:
-                raise Exception(
-                    "Cannot generate password under given conditions. "\
-                    "Change the parameters and try again")
+                raise ValueError(
+                    "Cannot generate password under given conditions.\
+Change the parameters and try again"
+                )
 
         return password
+
+    def _get_characters(self):
+        # If alphanumeric is set, we only want alphanumeric characters
+        if self.alphanumeric:
+            self.punctuation_occurrences = 0
+            self.space_occurrences = 0
+            return list(string.ascii_letters + string.digits)
+
+        # If safe_chars_only is set, we only want alphanumeric and safe_chars
+        if self.use_safe_chars:
+            self.space_occurrences = 0
+            return list(
+                string.ascii_letters + string.digits + "".join(self.safe_chars)
+            )
+
+        # Default where we include specified occurrences of character groups
+        characters = []
+        if self.number_occurrences > 0:
+            characters += list(string.digits)
+        if self.uppercase_occurrences > 0:
+            characters += list(string.ascii_uppercase)
+        if self.lowercase_occurrences > 0:
+            characters += list(string.ascii_lowercase)
+        if self.punctuation_occurrences > 0:
+            characters += list(string.punctuation)
+        if self.space_occurrences > 0:
+            characters.append(" ")
+
+        # Remove characters from excluded groups
+        for group_name in self.exclude_groups:
+            group_attr = getattr(self, group_name, None)
+            if group_attr:
+                characters = [x for x in characters if x not in group_attr]
+
+        return characters
+
     def _check_password(self, password):
-        punctuations = 0
-        numbers=0
-        uppercase = 0
-        lowercase = 0
-        spaces = 0
+        punctuations = sum(1 for x in password if x in string.punctuation)
+        numbers = sum(1 for x in password if x in string.digits)
+        lowercase = sum(1 for x in password if x in string.ascii_lowercase)
+        uppercase = sum(1 for x in password if x in string.ascii_uppercase)
+        spaces = password.count(" ")
 
-        #Todo: Use sum(comprehention) instead of looping through chars
-        # https://docs.python.org/3/library/secrets.html
-        for x in password:
-            punctuations += len([y for y in string.punctuation if x==y])
-            numbers += len([y for y in string.digits if x==y])
-            lowercase += len ([y for y in string.ascii_lowercase if x==y])
-            uppercase += len ([y for y in string.ascii_uppercase if x==y])
-            spaces += 1 if x == " " else 0 
+        return all(
+            [
+                punctuations >= self.punctuation_occurrences,
+                numbers >= self.number_occurrences,
+                lowercase >= self.lowercase_occurrences,
+                uppercase >= self.uppercase_occurrences,
+                spaces >= self.space_occurrences,
+            ]
+        )
 
-        if punctuations < self.punctuation_occurrences:
-            return False
-        if numbers < self.number_occurrences:
-            return False
-        if lowercase < self.lowercase_occurrences:
-            return False
-        if uppercase < self.uppercase_occurrences:
-            return False
-        if spaces < self.space_occurrences:
-            return False
 
-        return True
- 
 if __name__ == "__main__":
-    size = 20
-    exclude_characters = ""
+    parser = argparse.ArgumentParser(description="Generate a random password.")
 
-    if len(sys.argv) > 1 :
-        size = int(sys.argv[1])
-    if len(sys.argv) > 2:
-        exclude_characters = sys.argv[2]
-    pass_gen = RandomPassword(
-        length=size,
-        #exclude_characters=exclude_characters,
-        punctuation_occurrences=3
+    parser.add_argument(
+        "-l",
+        "--length",
+        type=int,
+        default=20,
+        help="The length of the password.",
     )
-    p = pass_gen.generate()
-    print (p)
+    parser.add_argument(
+        "--exclude", nargs="*", help="List of punctuation groups to exclude."
+    )
+
+    parser.add_argument(
+        "--alphanumeric",
+        action="store_true",
+        help="Create an alphanumeric password.",
+    )
+
+    parser.add_argument(
+        "--use-safe-chars",
+        action="store_true",
+        help="Use only alphanumeric characters and safe characters (- and _).",
+    )
+
+    # Add number of punctionation occurrences
+    parser.add_argument(
+        "--punctuation-occurrences",
+        type=int,
+        default=4,
+        help="Number of punctuations in the password.",
+    )
+
+    args = parser.parse_args()
+
+    pass_gen = RandomPassword(
+        length=args.length,
+        punctuation_occurrences=args.punctuation_occurrences,
+        exclude_groups=args.exclude if args.exclude else [],
+        alphanumeric=args.alphanumeric,
+        use_safe_chars=args.use_safe_chars,
+    )
+    print(pass_gen.generate())
